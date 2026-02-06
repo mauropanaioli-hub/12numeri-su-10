9727258609860ab12acfffe2c37a8c70a8e6ab6f
Mauro Panaioli <mauro.panaioli@gmail.com>
2026-02-06 20:09:59 +0100

    Add main.py entry point for python-for-android

---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Entry point richiesto da python-for-android (cerca main.py).
+# Avvia l'app Kivy definita in main_android.py.
+
+if __name__ == '__main__':
+    from main_android import App10eLottoAndroid
+    App10eLottoAndroid().run()
